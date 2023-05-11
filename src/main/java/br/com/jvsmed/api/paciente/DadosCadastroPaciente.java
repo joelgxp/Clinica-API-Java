a5baa46b1c42c91aac0bfb8388253eda76eab131
Joel <joelvieirasouza@gmail.com>
2023-05-11 01:35:18 -0300

    adicao classes

--- src/main/java/br/com/jvsmed/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/br/com/jvsmed/api/paciente/DadosCadastroPaciente.java
@@ -3,24 +3,45 @@ package br.com.jvsmed.api.paciente;
 import br.com.jvsmed.api.cnh.Categoria;
 import br.com.jvsmed.api.cnh.Solicitacao;
 import br.com.jvsmed.api.endereco.Endereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroPaciente(
+        @NotBlank
         String guia,
+        @NotBlank
         String registro,
+        @NotNull
         Solicitacao solicitacao,
+        @NotNull
         Categoria categoria,
+        @NotBlank
         String dataCadastro,
+        @NotBlank
         String dataHabilitacao,
+        @NotBlank
         String nome,
+        @NotBlank
         String dataNascimento,
-        String sexo,
+        @NotNull
+        Sexo sexo,
+        @NotBlank
+        @Pattern(regexp = "\\d-{11}")
         String cpf,
+        @NotBlank
         String nacionalidade,
+        @NotBlank
         String uf,
+        @NotBlank
         String nomeMae,
         String nomePai,
-        Endereco endereco
+        @NotNull
+        @Valid
+        Endereco endereco,
+        @NotBlank
+        String telefone
 
-
-        ) {
+) {
 }
---
+++ src/main/java/br/com/jvsmed/api/paciente/Sexo.java
@@ -0,0 +1,9 @@
+package br.com.jvsmed.api.paciente;
+
+public enum Sexo {
+    MASCULINO("Masculino"),
+    FEMININO("Feminino");
+
+    Sexo(String sexo) {
+    }
+}
