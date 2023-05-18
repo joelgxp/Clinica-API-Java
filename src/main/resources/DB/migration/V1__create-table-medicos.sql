487e208961369bd8f0d63e60f5d1f3bc15dc7f61
Samir <samircd9@gmail.com>
2023-05-18 20:26:04 -0300

    Correção arquivos sql

--- src/main/resources/DB/migration/V1__create-table-medicos.sql
+++ src/main/resources/DB/migration/V1__create-table-medicos.sql
@@ -1,11 +1,9 @@
-create database vollmed_api;
-
-create table medicos(
+create table medicos (
 
     id bigint not null auto_increment primary key,
     nome varchar(100) not null,
     email varchar(100) not null unique,
-    telefone varchar(20) not null;
+    telefone varchar(20) not null,
     crm varchar(6) not null unique,
     especialidade varchar(100) not null
 );
