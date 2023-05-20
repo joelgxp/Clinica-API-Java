210ee85836329d185f18ccd3f0edaeccf00a885a
Joel <joelvieirasouza@gmail.com>
2023-05-20 18:46:43 -0300

    adicao campo especialidade em Medico

--- src/main/java/br/com/jvsmed/api/medico/DadosListagemMedico.java
+++ src/main/java/br/com/jvsmed/api/medico/DadosListagemMedico.java
@@ -1,8 +1,20 @@
 package br.com.jvsmed.api.medico;
 
-public record DadosListagemMedico(Long id, String nome, String email, String crm, boolean ativo) {
+public record DadosListagemMedico(Long id,
+                                  String nome,
+                                  String email,
+                                  String crm,
+                                  String telefone,
+                                  Especialidade especialidade,
+                                  boolean ativo) {
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getAtivo());
+        this(medico.getId(),
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getTelefone(),
+                Especialidade.valueOf(String.valueOf(medico.getEspecialidade())),
+                medico.getAtivo());
     }
 }
