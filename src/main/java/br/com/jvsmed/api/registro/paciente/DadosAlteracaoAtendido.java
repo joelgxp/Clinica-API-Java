02e15c87ede986de49d3fa5af875d822b9954a62
Joel <joelvieirasouza@gmail.com>
2023-06-11 19:24:05 -0300

    alteracoes em Fila e Ficha de atendimento

--- src/main/java/br/com/jvsmed/api/registro/paciente/DadosAlteracaoAtendido.java
+++ src/main/java/br/com/jvsmed/api/registro/paciente/DadosAlteracaoAtendido.java
@@ -1,19 +1,10 @@
 package br.com.jvsmed.api.registro.paciente;
 
-import br.com.jvsmed.api.enums.ECategoria;
-import br.com.jvsmed.api.enums.ENacionalidade;
-import br.com.jvsmed.api.enums.ESexo;
-import br.com.jvsmed.api.enums.ESolicitacao;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
-import java.util.Date;
-
 
 public record DadosAlteracaoAtendido(
 
-        @Size(min = 11, max = 14, message = "CPF deve ter exatamente 11 dígitos")
+
         String cpf,
         boolean atendido
 
