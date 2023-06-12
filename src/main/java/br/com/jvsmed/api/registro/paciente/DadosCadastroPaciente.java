a4c3d68d60e083b5f5e62cf5c9239ec69174c41e
Joel <joelvieirasouza@gmail.com>
2023-06-11 21:46:07 -0300

    alteracoes em Fila e Ficha de atendimento

--- src/main/java/br/com/jvsmed/api/registro/paciente/DadosCadastroPaciente.java
+++ src/main/java/br/com/jvsmed/api/registro/paciente/DadosCadastroPaciente.java
@@ -7,12 +7,7 @@ import br.com.jvsmed.api.enums.ESolicitacao;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Date;
-
-
-
 public record DadosCadastroPaciente(
-
         @NotBlank
         String guia,
         @NotBlank
