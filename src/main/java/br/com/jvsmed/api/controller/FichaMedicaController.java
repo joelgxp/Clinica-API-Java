bdb2a1ac2cea902a900c5335ec6a0bfcd04bc3ae
Joel <joelvieirasouza@gmail.com>
2023-06-14 10:21:15 -0300

    melhorias no front

--- src/main/java/br/com/jvsmed/api/controller/FichaMedicaController.java
+++ src/main/java/br/com/jvsmed/api/controller/FichaMedicaController.java
@@ -1,6 +1,4 @@
 package br.com.jvsmed.api.controller;
-
-
 import br.com.jvsmed.api.entities.FichaMedicaEntity;
 import br.com.jvsmed.api.registro.fichaMedica.DadosCadastroFichaMedica;
 import br.com.jvsmed.api.repositories.FichaMedicaRepository;
@@ -8,7 +6,6 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 @CrossOrigin("*")
 @RestController
 @RequestMapping("ficha_medica")
