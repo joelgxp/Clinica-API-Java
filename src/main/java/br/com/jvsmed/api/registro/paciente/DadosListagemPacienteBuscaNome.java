05de525fefd87ad82778029fdbbbfef1a6cc103b
Joel <joelvieirasouza@gmail.com>
2023-06-18 20:16:48 -0300

    alteracoes no Front

--- src/main/java/br/com/jvsmed/api/registro/paciente/DadosListagemPacienteBuscaNome.java
+++ src/main/java/br/com/jvsmed/api/registro/paciente/DadosListagemPacienteBuscaNome.java
@@ -1,11 +1,6 @@
 package br.com.jvsmed.api.registro.paciente;
 
-
 import br.com.jvsmed.api.entities.PacienteEntity;
-import br.com.jvsmed.api.enums.ECategoria;
-import br.com.jvsmed.api.enums.ENacionalidade;
-import br.com.jvsmed.api.enums.ESexo;
-import br.com.jvsmed.api.enums.ESolicitacao;
 
 public record DadosListagemPacienteBuscaNome(
         Long id,
