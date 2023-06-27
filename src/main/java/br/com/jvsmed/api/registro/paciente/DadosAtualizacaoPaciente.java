a863ed792b2add4dc3ba1d36c401c4618f34ee1e
Joel <joelvieirasouza@gmail.com>
2023-06-26 23:10:27 -0300

    melhorias

--- src/main/java/br/com/jvsmed/api/registro/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/br/com/jvsmed/api/registro/paciente/DadosAtualizacaoPaciente.java
@@ -9,9 +9,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
-
-import java.sql.Time;
-
 public record DadosAtualizacaoPaciente(
         @NotBlank
         String guia,
