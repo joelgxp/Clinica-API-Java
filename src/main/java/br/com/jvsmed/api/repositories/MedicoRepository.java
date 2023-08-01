8fb262d99a26d4ad7e8d7a7c9fed6bbeac496b65
Joel <joelvieirasouza@gmail.com>
2023-07-31 23:19:22 -0300

    melhorias

--- src/main/java/br/com/jvsmed/api/repositories/MedicoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.jvsmed.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
-
-//    Page<MedicoEntity> findAllByAtivoTrue(Pageable paginacao);
-}
